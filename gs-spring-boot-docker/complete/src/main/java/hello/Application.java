c4bba68b72e56b4bef59ecc33249c39293042680
Annie Gu <ping.gu@emory.edu>
2018-03-22 17:47:05 -0400

    kong_apacheDS_integration: Added test scripts for request transformer

--- gs-spring-boot-docker/complete/src/main/java/hello/Application.java
+++ gs-spring-boot-docker/complete/src/main/java/hello/Application.java
@@ -12,21 +12,51 @@ import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.Map;
+
+
 @SpringBootApplication
 @RestController
 public class Application {
+    private static final Logger logger = LoggerFactory.getLogger(Application.class);
 
   private static final DateFormat sdf = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
   private static final DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy/MM/dd HH:mm:ss");
 
     @RequestMapping("/")
-    public String home() {
+    public String home(HttpServletRequest request, 
+        HttpServletResponse response) {
+        logger.info(":home(): invoked");
+        getHeadersInfo(request);
         Calendar cal = Calendar.getInstance();
         return "Backend Resource Response at: " + sdf.format(cal.getTime()) + System.getProperty("line.separator");
     }
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    private static Map<String, String> getHeadersInfo(HttpServletRequest request) {
+
+        Map<String, String> map = new HashMap<String, String>();
+
+        Enumeration headerNames = request.getHeaderNames();
+        while (headerNames.hasMoreElements()) {
+            String key = (String) headerNames.nextElement();
+            String value = request.getHeader(key);
+            logger.info(":getHeadersInfo(): key=" + key + ", value=" + value);
+            map.put(key, value);
+        }
+
+        return map;
     }
 
 }
---
+++ integration_tests/request_transformer/quick_test.sh
@@ -0,0 +1,14 @@
+## for quick start
+
+source ../common_functions.sh
+source ./config.sh
+
+set -x
+
+curl -i -X GET \
+  --url ${kong_api_url} \
+  --header "Host: ${hosts}" 
+
+  sleep 2
+  
+  docker logs kong-backend
