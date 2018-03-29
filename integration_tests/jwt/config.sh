8488c913dd0891185a04a25428e01dc7c8343377
Annie Gu <ping.gu@emory.edu>
2018-03-29 13:03:57 -0400

    migrage_to_kong_v_0.13.X: automated scripts for jwt module

--- integration_tests/jwt/config.sh
+++ integration_tests/jwt/config.sh
@@ -1,54 +1,42 @@
 #!/usr/bin/env bash
 
+source ./env
 
-source ../common_functions.sh
-
-export module=jwt
-export name=${module}_srv
-export url=${upstream_url}
-export hosts=${module}.org
-export consumerName=${module}_consumer
-##export key=${consumerName}_key
-##export secret=${consumerName}_secret
-##export algorithm=HS256
-
-export key=cef1fe6937e444a6b18a26965d619718
-export secret=b9f813fb8753440eabb1b44f9ba4da2f
-export algorithm=HS256
-
+##########################################################
 echo "Adding Service: name=${name}, url=${url}"
 curl -i -X POST \
       --url ${kong_admin_url}/services/    \
       --data "name=${name}"                 \
       --data "url=${url}"
 
+sleep 2
+
+##########################################################
 echo "Add JWT plugin on top of a Service ${name}"
 curl -i -X POST \
       --url ${kong_admin_url}/services/${name}/plugins \
       --data "name=jwt"
 
-echo "Adding Route for service:${name} with hosts: ${hosts}"
-curl -i -X POST \
-      --url ${kong_admin_url}/services/${name}/routes    \
-      --data "hosts[]=${hosts}"
+sleep 2
 
-echo "Input route id (id from the previous response), followed by [ENTER]:"
-route_id=$(getUserInput)
+##########################################################
+echo "Adding Route for service:${name} with hosts: ${hosts}"
+route_id=$(createRoute ${name} ${hosts})
 echo "route id is ${route_id}"
 
+sleep 2
+
+##########################################################
 echo "Enable JWT plugin for Route "
 curl -i -X POST \
       --url ${kong_admin_url}/routes/${route_id}/plugins       \
       --data "name=jwt"
 
-echo "Create a Consumer"
-curl -i -X POST \
-      --url ${kong_admin_url}/consumers/         \
-      --data "username=${consumerName}"          \
-      --data "custom_id=${consumerName}_id"
+sleep 2
 
-echo "Input consumer id (id from the previous response), followed by [ENTER]:"
-consumer_id=$(getUserInput)
+##########################################################
+echo "Create a Consumer"
+consumer_id=$(createConsumer ${consumerName})
 echo "consumer id is ${consumer_id}"
 
 echo "Create a JWT credential for Consumer ${consumerName}, consumer id: ${consumer_id}"
@@ -57,3 +45,5 @@ curl -i -X POST \
       -d "secret=${secret}&algorithm=${algorithm}&key=${key}" \
       -H "Content-Type: application/x-www-form-urlencoded"
 
+##########################################################
+echo "To test: ./test.sh"
--- integration_tests/jwt/test.sh
+++ integration_tests/jwt/test.sh
@@ -1,20 +1,6 @@
 #!/usr/bin/env bash
 
-
-source ../common_functions.sh
-
-export module=jwt
-export name=${module}_srv
-export url=${upstream_url}
-export hosts=${module}.org
-export consumerName=${module}_consumer
-##export key=${consumerName}_key
-##export secret=${consumerName}_secret
-##export algorithm=HS256
-
-export key=cef1fe6937e444a6b18a26965d619718
-export secret=b9f813fb8753440eabb1b44f9ba4da2f
-export algorithm=HS256
+source ./env
 
 set -x
 jwt=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJjZWYxZmU2OTM3ZTQ0NGE2YjE4YTI2OTY1ZDYxOTcxOCIsImlhdCI6bnVsbCwiZXhwIjpudWxsLCJhdWQiOiIiLCJzdWIiOiIifQ.50bnA9d3wjIYIx6m-fzxIHQHfXF6zuJRPMUdk8-4LTU
@@ -25,6 +11,7 @@ curl -i -X  GET \
       --header "Authorization: Bearer ${jwt}" \
       --header "Host: ${hosts}"
 
+exit
 
 echo ""
 echo "The plugin will append some headers to the request"
