add6b3b4399beefc3ae637eece46b3246eb00f13
Pradeeban Kathiravelu <kk.pradeeban@gmail.com>
2020-07-01 15:24:32 -0400

    fix password missing error

--- install_postgres.sh
+++ install_postgres.sh
@@ -7,6 +7,7 @@ docker run -d --name kong-database \
               -p 5432:5432 \
               -e "POSTGRES_USER=kong" \
               -e "POSTGRES_DB=kong" \
+-e POSTGRES_HOST_AUTH_METHOD=trust \
               postgres:${POSTGRES_VERSION}
 
 
